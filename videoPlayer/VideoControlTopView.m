0caa78c580b7276925ee4ade0a59bd9eff4661f9
WymanY <wuyun768@icloud.com>
2016-02-29 23:23:20 +0800

    添加了返回箭头

--- videoPlayer/VideoControlTopView.m
+++ videoPlayer/VideoControlTopView.m
@@ -12,6 +12,8 @@
 
 @interface VideoControlTopView ()
 @property (nonatomic,strong) UIButton *navBackIndicatorBtn;
+@property (nonatomic,strong) UILabel *titleLabel;
+@property (nonatomic,strong) UIImageView *logoImagview;
 @end
 
 @implementation VideoControlTopView
@@ -22,16 +24,42 @@
         
         //添加返回 Button
         UIButton *navBackIndicatorBtn = [UIButton new];
-        [navBackIndicatorBtn setImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
-        [navBackIndicatorBtn setImage:[UIImage imageNamed:@"player_back"] forState:UIControlStateHighlighted];
+        [navBackIndicatorBtn setImage:[UIImage imageNamed:@"back2"] forState:UIControlStateNormal];
         [self addSubview:navBackIndicatorBtn];
         self.navBackIndicatorBtn = navBackIndicatorBtn;
         
         [navBackIndicatorBtn mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(15);
             make.centerY.mas_equalTo(0);
+            make.size.mas_equalTo(CGSizeMake(52, 34));
         }];
         
+        //添加右边的 Logo
+        
+        UIImageView *logoImageView = [UIImageView new];
+        [self addSubview:logoImageView];
+        logoImageView.image = [UIImage imageNamed:@"player_logo_full"];
+        self.logoImagview = logoImageView;
+        
+        [logoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(60, 40));
+            make.right.mas_equalTo(-5);
+            make.top.mas_equalTo(0);
+        }];
+        
+        
+        //添加标题按钮
+        UILabel *titleLabel = [UILabel new];
+        titleLabel.textColor = [UIColor whiteColor];
+        titleLabel.text = @"我是天下第一帅的武藴牛x";
+        [self addSubview:titleLabel];
+        self.titleLabel = titleLabel;
+        [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(navBackIndicatorBtn.mas_right).offset(5);
+            make.right.mas_equalTo(logoImageView.mas_left);
+            make.top.bottom.equalTo(self);
+
+        }];
     }
     return self;
 }
